e020706cc838f16b568a73271bfc80ac35e5c953
Umair Abdus Salam <umairabdsalam@gmail.com>
2024-06-12 23:48:39 +0100

    Initial Commit

---
+++ SwiftUICalculator/SwiftUICalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUICalculatorApp.swift
+//  SwiftUICalculator
+//
+//  Created by Umair Salam on 6/12/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUICalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
